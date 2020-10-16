3c6f333caa31e61a906e21e608036f8a9080ed40
piiiiva <piiiiva@gmail.com>
2020-10-16 17:55:30 -0300

    Create landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import { FiArrowRight } from 'react-icons/fi';
+
+import './styles/global.css'
+import './styles/pages/landing.css'
+
+import logoImg from './images/logo.svg'
 
 interface TitleProps {
   text: string;
@@ -12,12 +18,24 @@ function Title(props: TitleProps) {
 
 function App() {
   return (
-    <div className="App">
-      <Title text="Título 1"/>
-      <Title text="Título 2"/>
-      <Title text="Título 3"/>
-      <Title text="Título 4"/>
-      <Title text="Título 5"/>
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Happy logo"/>
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>Visite orfanatos e mude o dia de muitas crianças.</p>
+        </main>
+        
+        <div className="location">
+          <strong>Rio do Sul</strong>
+          <span>Santa Catarina</span>
+        </div>
+
+        <a href="" className="enter-app">
+          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
+        </a>
+      </div>
     </div>
   );
 }
