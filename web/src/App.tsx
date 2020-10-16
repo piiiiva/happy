aab12c42167d343ead3e0a5650af98908ed9a6e1
piiiiva <piiiiva@gmail.com>
2020-10-16 16:13:59 -0300

    react props and interfaces in typescript

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
+interface TitleProps {
+  text: string;
+}
+
+function Title(props: TitleProps) {
+  return (
+    <h1>{props.text}</h1>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <h1>Hello World Happy!</h1>
+      <Title text="Título 1"/>
+      <Title text="Título 2"/>
+      <Title text="Título 3"/>
+      <Title text="Título 4"/>
+      <Title text="Título 5"/>
     </div>
   );
 }
